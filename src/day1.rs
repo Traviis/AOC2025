30da6415dddc0f221bac8b8b3bc1a4b7bad53ab1
Travis Taylor <travis@trickfire.com>
2025-11-26 20:38:45 -0700

    Roll back to empty repo

--- src/day1.rs
+++ src/day1.rs
@@ -5,46 +5,17 @@ type OutputType = i32;
 
 #[aoc_generator(day1)]
 fn day1_parse(input: &str) -> InputType {
-    input
-        .lines()
-        .map(|line| {
-            let mut iter = line.split_whitespace();
-            (
-                iter.next().unwrap().parse::<i32>().unwrap(),
-                iter.next().unwrap().parse::<i32>().unwrap(),
-            )
-        })
-        .unzip()
+    todo!();
 }
 
 #[aoc(day1, part1)]
 pub fn part1(input: &InputType) -> OutputType {
-    let mut left = input.0.clone();
-    let mut right = input.1.clone();
-
-    left.sort();
-    right.sort();
-
-    left.iter()
-        .zip(right.iter())
-        .map(|(l, r)| (l - r).abs())
-        .sum::<i32>()
+    todo!();
 }
 
 #[aoc(day1, part2)]
 pub fn part2(input: &InputType) -> OutputType {
-    let left = &input.0;
-    //Convert to a count of the number of times each element appears
-    let right = &input.1;
-
-    let dup_list = right.iter().fold(BTreeMap::new(), |mut acc, x| {
-        *acc.entry(x).or_insert(0) += 1;
-        acc
-    });
-
-    left.iter()
-        .map(|l| dup_list.get(l).unwrap_or(&0) * l)
-        .sum::<i32>()
+    todo!();
 }
 
 #[cfg(test)]
