b3701d1adc6e8ff98c836bb64f8fc5b3315e1cd8
Travis Taylor <travis@trickfire.com>
2025-12-01 17:53:25 -0700

    feat: Day 1, part 2
    fix: I got lucky, but technically % 100 doesn't quite work as expected.
    Use the euclid remainder.

--- src/day1.rs
+++ src/day1.rs
@@ -43,8 +43,8 @@ pub fn part1(input: &InputType) -> OutputType {
 
     for dial_move in input.iter() {
         position = match dial_move {
-            DialAction::L(n) => (position - n) % 100,
-            DialAction::R(n) => (position + n) % 100
+            DialAction::L(n) => (position - n).rem_euclid(100),
+            DialAction::R(n) => (position + n).rem_euclid(100)
         };
         if position == 0 {
             zeroes = zeroes + 1;
@@ -57,7 +57,30 @@ pub fn part1(input: &InputType) -> OutputType {
 
 #[aoc(day1, part2)]
 pub fn part2(input: &InputType) -> OutputType {
-    todo!();
+    let mut position = 50;
+    let mut zero_passes= 0;
+
+    for dial_move in input.iter() {
+        position = match dial_move {
+            DialAction::L(n) => {
+                // You can't use standard logic here for division. We also need to account for the
+                // case where we are starting at 0, and we take -1 from the position from both the
+                // start and end to see if we pass over 0. The logic is simply confusing when you
+                // start at 0 at the start of an instruction
+                let start_gen = (position - 1 as i32).div_euclid(100);
+                let end_gen = (position- n - 1).div_euclid(100);
+                
+                zero_passes += (start_gen - end_gen).abs();
+                (position - n).rem_euclid(100)
+            },
+            DialAction::R(n) => {
+                zero_passes += (position + n) / 100;
+                (position + n).rem_euclid(100)
+            }
+        };
+
+    }
+    zero_passes
 }
 
 #[cfg(test)]
@@ -84,7 +107,12 @@ L82"
     }
 
     #[test]
+    fn day1_matchcheck() {
+        assert_eq!((-1i32).rem_euclid(100), 99);
+    }
+
+    #[test]
     fn day1_part2() {
-        assert_eq!(part2(&day1_parse(get_test_input())), 31);
+        assert_eq!(part2(&day1_parse(get_test_input())), 6);
     }
 }
